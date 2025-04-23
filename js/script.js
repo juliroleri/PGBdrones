352141134e59db65198b1af71b398a6ab0f738c1
juliroleri <julianaroleri@gmail.com>
2025-04-23 16:22:00 -0300

    script.js

--- js/script.js
+++ js/script.js
@@ -36,42 +36,7 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 
-const galeria = document.getElementById('galeria');
-const btnIzquierda = document.getElementById('btnIzquierda');
-const btnDerecha = document.getElementById('btnDerecha');
-const scrollAmount = 450;
-
-function scrollGaleria(direccion) {
-  galeria.scrollBy({
-    left: direccion * scrollAmount,
-    behavior: 'smooth'
+document.getElementById('menuToggle').addEventListener('click', function () {
+    document.getElementById('menu').classList.toggle('active');
   });
-
-  setTimeout(actualizarFlechas, 400);
-}
-
-function actualizarFlechas() {
-  const scrollLeft = galeria.scrollLeft;
-  const scrollWidth = galeria.scrollWidth;
-  const clientWidth = galeria.clientWidth;
-
-  btnIzquierda.classList.toggle('oculto', scrollLeft <= 0);
-  btnDerecha.classList.toggle('oculto', scrollLeft + clientWidth >= scrollWidth - 5);
-}
-
-btnIzquierda.addEventListener('click', () => scrollGaleria(-1));
-btnDerecha.addEventListener('click', () => scrollGaleria(1));
-
-
-window.addEventListener('load', actualizarFlechas);
-window.addEventListener('resize', actualizarFlechas);
-galeria.addEventListener('scroll', actualizarFlechas);
-
-
-const menuIcon = document.getElementById("menu-icon");
-const menu = document.getElementById("menu");
-
-// Cuando el ícono de hamburguesa sea clickeado, toggle la clase 'active' para mostrar/ocultar el menú
-menuIcon.addEventListener("click", () => {
-    menu.classList.toggle("active");
-});
+  
